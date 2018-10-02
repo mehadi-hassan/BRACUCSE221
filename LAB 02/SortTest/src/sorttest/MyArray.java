a48782b0f4636486396438dd8046a588c130c4db
Mehadi Hassan <mail@mehadihassan.com>
2018-10-02 04:46:31 +0000

    Update MyArray.java

--- LAB 02/SortTest/src/sorttest/MyArray.java
+++ LAB 02/SortTest/src/sorttest/MyArray.java
@@ -133,7 +133,40 @@ public class MyArray {
         
     }
     
-    
+    public static void heapSort(int[] a){
+       int n = a.length;
+        for (int i = n/2 - 1 ; i >= 0 ; i--){ 
+            heapify(a, n, i);
+        }
+        
+        for (int i=n-1; i>=0; i--){
+            int temp = a[0]; 
+            a[0] = a[i]; 
+            a[i] = temp;
+            heapify(a, i, 0); 
+        } 
+   }
+   
+    public static void heapify(int arr[], int n, int i) 
+    { 
+        int largest = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+        
+        if (l < n && arr[l] > arr[largest]){ 
+            largest = l;
+        }
+        
+        if (r < n && arr[r] > arr[largest]) 
+            largest = r; 
+  
+        if (largest != i){ 
+            int swap = arr[i]; 
+            arr[i] = arr[largest]; 
+            arr[largest] = swap;
+            heapify(arr, n, largest); 
+        } 
+    } 
     
     public static int findK(int[] a, int start, int end, int k){
         int index = partition (a , start , end);
